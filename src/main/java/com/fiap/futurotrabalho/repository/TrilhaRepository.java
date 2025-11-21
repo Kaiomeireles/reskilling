be873da7130c6833ca3764a89baa2c5d2f378c68
Kaio Meireles <145077870+Kaiomeireles@users.noreply.github.com>
2025-11-21 10:45:02 -0500

    Add files via upload

---
+++ src/main/java/com/fiap/futurotrabalho/repository/TrilhaRepository.java
@@ -0,0 +1,7 @@
+package com.fiap.futurotrabalho.repository;
+
+import com.fiap.futurotrabalho.model.TrilhaAprendizagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrilhaRepository extends JpaRepository<TrilhaAprendizagem, Long> {
+}
